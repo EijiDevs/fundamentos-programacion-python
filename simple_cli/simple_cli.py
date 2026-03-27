f9b77e655e066c549f04d82cd42acb64f12e9412
Esteban Perafan <bestebanperafan@gmail.com>
2026-03-27 09:30:20 -0300

    docs: corrección del nombre de referencia de la libreria Simple CLI (antes SimpleTui) en documentación de ejemplo)

--- simple_cli/simple_cli.py
+++ simple_cli/simple_cli.py
@@ -100,10 +100,10 @@ def _request_input(message):
     return input(f"{message}: ")
 
 if __name__ == "__main__":
-    print("Bienvenido a SimpleTui! Somos una libreria diseñada para gestionar interfaces de usuario en terminal de forma simple.\n")
+    print("Bienvenido a Simple CLI! Somos una libreria diseñada para gestionar interfaces de usuario en terminal de forma simple.\n")
     print("Ej:")
     print("")
-    print("from SimpleTui import show_header")
+    print("from simple_cli import show_header")
     print('show_header(message="Hola mundo!")')
     print("")
     print("Resultado:")
