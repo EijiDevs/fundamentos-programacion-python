97494ecfd24c0a2547ec8978490a9f72aa3bbd40
Esteban Perafan <bestebanperafan@gmail.com>
2026-03-27 21:34:57 -0300

    feat(time): se importa time y se agrega sleep de 2 segundos para intercambio de interfaces

--- actividad-2/pos.py
+++ actividad-2/pos.py
@@ -1,5 +1,20 @@
-from simple_cli import header, menu
+from simple_cli import header, menu, clear
 from enum import Enum
+import time
+
+class POS(Enum):
+    CREAR_PRODUCTO = 1
+    VER_PRODUCTO = 2
+    VER_PRODUCTOS = 3
+    ELIMINAR_PRODUCTO = 4
+    ABASTECER_STOCK_PRODUCTO = 5
+    VER_STOCK_PRODUCTOS = 6
+    VER_STOCK_PRODUCTO = 7
+    CREAR_CLIENTE = 8
+    VER_CLIENTE = 9
+    VER_CLIENTES = 10
+    ELIMINAR_CLIENTE = 11
+    VENDER = 12
 
 class MetodoPago(Enum):
     EFECTIVO = 1
@@ -7,11 +22,19 @@ class MetodoPago(Enum):
     CREDITO = 3
     TRANSFERENCIA = 4
 
+
+clientes = [] # Codigo, RUT, Nombre, Apellido, Correo
+productos = [] # Codigo, Nombre, Descripcion, Stock, Precio
+ventas = [] # Codigo, Codigo Cliente, Codigo Producto, Cantidad, Total, Metodo Pago, Fecha Pago, Hora Pago
+
 if __name__ == "__main__":
     
     medio_pago_seleccionado = None
 
     header(message="Hola mundo! en POS")
+
+    time.sleep(2)
+    clear()
 
     try:
         medio_pago_seleccionado = MetodoPago(menu(
@@ -19,9 +42,7 @@ if __name__ == "__main__":
             options=MetodoPago,
             input_message="Por favor, ingrese su medio de pago"
         ))
-    except:
-        pass
-
-    print(medio_pago_seleccionado)
+    except Exception as err:
+        raise err
 
 
