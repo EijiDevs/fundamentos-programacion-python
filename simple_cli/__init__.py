1bffee0b6636f1bd624c965b297c3f5ea00d015e
Esteban Perafan <bestebanperafan@gmail.com>
2026-03-29 18:13:07 -0300

    refactor(simple_cli:imports): se ajustan los imports del paquete para evitar hacer referencias relativas para el uso de componentes

--- simple_cli/__init__.py
+++ simple_cli/__init__.py
@@ -7,6 +7,4 @@ Módulo para gestionar interfaces de línea de comandos de forma simple.
 __author__ = 'Esteban Perafan'
 __version__ = '1.0'
 
-from .components.header import header
-from .components.menu import menu
-from .components.clear import clear
+from .components import header, menu, clear
--- simple_cli/components/__init__.py
+++ simple_cli/components/__init__.py
@@ -1,1 +1,3 @@
-# Vacío, solo marca el directorio como paquete
+from .menu import menu
+from .header import header
+from .clear import clear
