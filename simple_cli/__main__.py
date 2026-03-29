bb87dfc1b9277050cb018d8fa6dd6b69b015c76b
Esteban Perafan <bestebanperafan@gmail.com>
2026-03-29 18:13:45 -0300

    docs(simple_cli): se agrega ejemplo de uso del componente 'menu'

--- simple_cli/__main__.py
+++ simple_cli/__main__.py
@@ -1,14 +1,15 @@
-from .components.header import header
+from .components import header, menu
 from .utils import _indent
 from textwrap import dedent
+from enum import Enum
 
 WELCOME_MESSAGE = dedent("""\
     Bienvenido a Simple CLI! 
-    Somos una libreria diseñada para gestionar interfaces de usuario en terminal de forma simple.
-""")
+    Somos una libreria diseñada para gestionar interfaces de usuario en terminal de forma simple.""")
 
 EXAMPLE_MESSAGE = dedent("""\
-    Ejemplos:
+    
+    Ejemplo:
     
     {code_example}
 """)
@@ -21,10 +22,40 @@ USE_HEADER_EXAMPLE = dedent("""\
     header("Hola mundo!")    
 """)
 
+USE_MENU_EXAMPLE = dedent("""\
+    # Imprimir un menu sin solicitar un entrada, haciendo uso de un Enum
+    from simple_cli import menu
+    
+    class EnumExample(Enum):
+        EXAMPLE_1 = 1
+        EXAMPLE_2 = 2
+        EXAMPLE_3 = 3
+        EXAMPLE_4 = 4
+    
+    menu(
+        message="Encabezado de ejemplo para menú",
+        values=EnumExample,
+        accept_input=False
+    )
+""")
+
 def main():
+    class EnumExample(Enum):
+        EXAMPLE_1 = 1
+        EXAMPLE_2 = 2
+        EXAMPLE_3 = 3
+        EXAMPLE_4 = 4
+
     print(WELCOME_MESSAGE)
     print(EXAMPLE_MESSAGE.format(code_example=_indent(USE_HEADER_EXAMPLE)))
     print(RESULT_MESSAGE)
     header("Hola mundo!")
+    print(EXAMPLE_MESSAGE.format(code_example=_indent(USE_MENU_EXAMPLE)))
+    print(RESULT_MESSAGE)
+    menu(
+        message="Encabezado de ejemplo para menú",
+        values=EnumExample,
+        accept_input=False
+    )
 
 if __name__ == "__main__": main()
