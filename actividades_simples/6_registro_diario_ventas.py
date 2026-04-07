edb3e83fc9e2bcbb978093959ce8c890577973d1
Esteban Perafan <bestebanperafan@gmail.com>
2026-04-06 23:23:49 -0400

    feat: se sube ejercicio 6 de registro de ventas diario

---
+++ actividades_simples/6_registro_diario_ventas.py
@@ -0,0 +1,47 @@
+
+opcion = None
+cantidad_ventas = 0
+total_ventas = 0
+promedio_venta = 0
+while opcion != 3:
+    print("=== Registro Diario de Ventas ===")
+    print("Por favor, seleccione una de las siguientes opciones del menú:")
+    print("1. Registrar Ventas")
+    print("2. Mostrar analisis de Ventas")
+    print("3. Salir")
+
+    opcion = int(input("Ingrese el número de la opción que desea seleccionar: "))
+
+    match opcion:
+        case 1:
+            print("Opción 1 seleccionada: Registrar Ventas")
+            cantidad_ventas = int(input("Por favor, ingrese la cantidad de ventas a registrar: "))
+            if cantidad_ventas <= 5:
+                 print("La cantidad de ventas a registrar debe ser mayor a 5. Por favor, intente nuevamente.")
+                 continue
+            for i in range(cantidad_ventas):
+                total_ventas += float(input(f"Ingrese el monto total de la venta #{i + 1} (Se aceptan decimales): "))
+        case 2:
+            print("Opción 2 seleccionada: Mostrar análisis de Ventas")
+            if cantidad_ventas == 0:
+                print("No se han registrado ventas aún. Por favor, registre ventas antes de mostrar el análisis.")
+                continue
+
+            promedio_venta = total_ventas / cantidad_ventas
+
+            print(f"Cantidad total de ventas registradas: {cantidad_ventas}")
+            print(f"Monto total de ventas registradas: {total_ventas}")
+            print(f"Promedio por venta: {promedio_venta}")
+
+            if promedio_venta > 50000:
+                print("=== ¡VENTAS SOBRE EL PROMEDIO ESPERADO! ===")
+            else:
+                print("=== ¡VENTAS BAJO EL PROMEDIO ESPERADO! ===")
+
+        case _:
+            opcion = None
+            cantidad_ventas = 0
+            total_ventas = 0
+            promedio_venta = 0
+            print("Gracias por utilizar el sistema de registro diario de ventas. ¡Hasta luego!")
+            print("Fin del registro...")
