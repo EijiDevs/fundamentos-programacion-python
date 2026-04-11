57082772af0a2adb28761fd386c90e7124dcf1f6
Esteban Perafan <bestebanperafan@gmail.com>
2026-04-10 20:58:48 -0400

    feat: se agrega ejercicio 7 de acceso simple

---
+++ actividades_simples/7_acceso_simple.py
@@ -0,0 +1,18 @@
+print("=== Sistema de acceso a sala ===")
+
+cupos_disponibles = int(input("Por favor, ingrese la cantidad de cupos disponibles en la sala \n:"))
+has_reserva = False
+
+print("Por favor, indique si tiene una reserva para ingresar a la sala")
+print("1. Sí, tengo una reserva")
+print("2. No, no tengo una reserva")
+has_reserva = True if (int(input("Indique una opción (1/2): ")) == 1) else False
+
+if cupos_disponibles <= 0:
+    print("Ingreso rechazado: sin cupos")
+    exit()
+
+if has_reserva:
+    print("Ingreso permitido")
+else:
+    print("Ingreso rechazado: sin reserva")
